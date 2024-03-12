fa146cdc3c9f194f0ee62c91935851cc23871e55
endricklosange <endricklosange@gmail.com>
2024-03-12 16:21:41 +0100

    feat(make pwa html)

---
+++ serviceWorker.js
@@ -0,0 +1,37 @@
+const PREFIX = 'V2';
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil((async () => {
+        const cache = await caches.open(PREFIX);
+        await cache.add(new Request('/offline.html'));
+    })());
+    console.log(`${PREFIX} install`);
+});
+self.addEventListener('activate', (event) => {
+    clients.claim();
+    event.waitUntil((async () => {
+        const keys = await caches.keys();
+        await Promise.all(keys.map((key) => {
+            if(key.includes(PREFIX)) {
+                return caches.delete(key);
+        }
+    }));
+    console.log(`${PREFIX} activate`);
+});
+self.addEventListener('fetch', (event) => {
+    console.log(`fetching : ${event.request.url}, mode : ${event.request.mode}`);
+    if (event.request.mode === 'navigate') {
+        event.respondWith((async () => {
+            try {
+                const preloadResponse = await event.preloadResponse;
+                if (preloadResponse) {
+                    return preloadResponse;
+                }
+                return await fetch(event.request);
+            } catch (e) {
+                const cache = await caches.open(PREFIX);
+                return await cache.match('/offline.html');
+            }
+        })());
+    }
+});
